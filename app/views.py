478cba6787ef387e31adcae59e06b509b12c2955
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-06 15:41:04 -0400

    created a variable and template tag

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
+from random import randint
 
 @app.route('/')
 def lucky_static():
-    return render_template('simple.html')
+    #template name comes before template variables
+    return render_template('simple.html',
+        lucky_num=randint(1, 100))
