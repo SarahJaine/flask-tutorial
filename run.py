18bd98e2ccc114a3f5c78072cd06b45d04d03cc6
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-06 15:22:25 -0400

    created app

--- run.py
+++ run.py
@@ -1,10 +1,3 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello world!"
-
-if __name__ == "__main__":
-    app.run()
+app.run()
